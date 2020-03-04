b0b0117d2616ccc0aa397d81983e785810423710
Cihat Duyku <>
2020-03-04 19:04:46 +0300

    log4j versiyonu yenilendi

--- src/test/java/com/cihat/trendyol/logger/Log.java
+++ src/test/java/com/cihat/trendyol/logger/Log.java
@@ -1,9 +1,10 @@
 package com.cihat.trendyol.logger;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Log {
-    private static Logger logger = Logger.getLogger(Log.class.getSimpleName());
+    private static final Logger logger = LogManager.getLogger(Log.class.getName());
 
 
     public static void info(String message) {
